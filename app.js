f65012bf7604e0a45416d70d1f707b28a2d9f8ae
Michael Oakley <Michael.Oakley@nrel.gov>
2015-03-10 14:41:12 -0600

    Changed API to port 8081

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ $(function() {
     var filters = countFilterGroups(' .checkbox-group' );
 
     //var jqxhr = $.getJSON('data.json');
-    var jqxhr = $.getJSON('http://xcomm2dev.nrel.gov:8080/api/Programs');
+    var jqxhr = $.getJSON('http://xcomm2dev.nrel.gov:8081/api/Programs');
 
     jqxhr.done( buildCards, initIsotope );
 
